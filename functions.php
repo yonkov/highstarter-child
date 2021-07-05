8d3c002e663a3bd185913230d8c70e22e2cbe180
Yonkov <yonkov.atanas@gmail.com>
2021-07-05 22:54:48 +0300

    remove parent theme enqueue and add css version

--- functions.php
+++ functions.php
@@ -1,11 +1,9 @@
 <?php
 
 function highstarter_child_enqueue_theme_styles() {
-    //Register parent theme's styles
-   wp_enqueue_style( 'highstarter-style', get_template_directory_uri().'/style.css' );
-   //Register child theme's styles
-   wp_enqueue_style('highstarter-child', get_stylesheet_directory_uri() .'/style.css');
+	// Register child theme's styles
+	wp_enqueue_style( 'highstarter-child', get_stylesheet_directory_uri() . '/style.css', array(), '1.0.0' );
 }
-add_action( 'wp_enqueue_scripts', 'highstarter_child_enqueue_theme_styles' );
+add_action( 'wp_enqueue_scripts', 'highstarter_child_enqueue_theme_styles', 999 );
 
 /* Add your child theme's code snippets below */
